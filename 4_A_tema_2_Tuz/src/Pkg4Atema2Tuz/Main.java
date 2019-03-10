b4db983cbadd9bb26a64831b614cc9d10ab8677c
Marco Alejandro Cupul Hau <marcoalejandro298@gmail.com>
2019-03-10 11:53:34 -0600

    Métodos de solución de ecuaciones

---
+++ 4_A_tema_2_Tuz/src/Pkg4Atema2Tuz/Main.java
@@ -0,0 +1,18 @@
+
+package Pkg4Atema2Tuz;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+       frmPrincipal frm = new frmPrincipal();
+       frm.setTitle("Metodos de Solución de Ecuaciones");
+       frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frm.setExtendedState(JFrame.MAXIMIZED_BOTH); //maximizar
+       frm.setLocationRelativeTo(null); //centrar
+       frm.setVisible(true);
+       
+    }
+    
+}
